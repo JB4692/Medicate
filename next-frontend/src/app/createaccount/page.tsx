d11cb05841eb190730f94ffcace46add4d25813b
Josh Brown <joshbrown4692@gmail.com>
2024-03-24 21:52:40 -0400

    consolidated all css into home.module.css

--- next-frontend/src/app/createaccount/page.tsx
+++ next-frontend/src/app/createaccount/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./createaccount.module.css";
+import styles from "../home.module.css";
 
 const CreateAccountPage = () => {
     return (
